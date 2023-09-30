f5f0ae243eaf6f0dd56439882db5e690fdb1971b
Kehinde Olaleye Simeon <simeonkenny66@gmail.com>
2023-09-30 12:54:45 +0100

    so

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import Navbar from "./components/Navbar";
+import fishStyle from "./simeon.module.css"
 
 const App = () => {
+  let age = 500;
+  let myStyle = {
+    color:"red",
+    backgroundColor:"white"
+  }
   return (
     <>
+      <h1 style={myStyle}>Hello Sim</h1>
+      <h1 className={fishStyle.fish}>Hello Sim</h1>
+      <h2 className="bg-danger">{age}</h2>
       <Navbar></Navbar>
-      <h1>Hello Sim</h1>
     </>
   );
 };
