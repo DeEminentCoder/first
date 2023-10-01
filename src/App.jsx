e45aaa8f326aa6c11583ec03af034ecf216df680
Kehinde Olaleye Simeon <simeonkenny66@gmail.com>
2023-10-01 22:39:32 +0100

    usestate

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,14 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import fishStyle from "./simeon.module.css"
+import { useState } from "react";
 
 const App = () => {
-  let age = 500;
-  let myStyle = {
-    color:"red",
-    backgroundColor:"white"
-  }
+  const [myNo, setMyNo] = useState(0)
   return (
     <>
-      <h1 style={myStyle}>Hello Sim</h1>
-      <h1 className={fishStyle.fish}>Hello Sim</h1>
-      <h2 className="bg-danger">{age}</h2>
-      <Navbar></Navbar>
+    <h1>{myNo}</h1>
+    <button onClick={()=>setMyNo(myNo+30)}>Increase</button>
     </>
   );
 };
