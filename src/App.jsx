0ddebf2490034fe1dea1e41ee44707d85c2bad9f
Kehinde Olaleye Simeon <simeonkenny66@gmail.com>
2023-10-02 12:53:49 +0100

    firstname

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,26 @@ import fishStyle from "./simeon.module.css"
 import { useState } from "react";
 
 const App = () => {
-  const [myNo, setMyNo] = useState(0)
+  // const [myNo, setMyNo] = useState(0)
+
+  const [fn, setfn] = useState("")
+  const [ln, setln] = useState("")
+  const [email, setemail] = useState("")
+  const [Password, setpassword] = useState("")
+  
   return (
     <>
-    <h1>{myNo}</h1>
-    <button onClick={()=>setMyNo(myNo+30)}>Increase</button>
+    {/* <h1>{myNo}</h1>
+    <button onClick={()=>setMyNo(myNo+30)}>Increase</button> */}
+
+    {/* SIMPLE SCHOOL PORTAL */}
+    <h1>Simozy Group Of Schools</h1>
+    <input type="text" placeholder="First Name" />
+    <input type="text" placeholder="Last Name" />
+    <input type="text" placeholder="Email" />
+    <input type="text" placeholder="Password" />
+    <hr />
+    <button onClick={}>Register</button>
     </>
   );
 };
