8880c30fdf5b8dd033ff8b5dac6e50830b602827
Kehinde Olaleye Simeon <simeonkenny66@gmail.com>
2023-10-03 14:21:30 +0100

    nav

---
+++ src/components/FirstEx.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { useState } from "react";
+
+const First = () => {
+  const [myNo, setMyNo] = useState(0)
+
+  const [fn, setfn] = useState("");
+  const [ln, setln] = useState("");
+  const [email, setemail] = useState("");
+  const [Password, setpassword] = useState("");
+  const [allstudents, setallstudents] = useState([])
+  const register = () => {
+    console.log(fn, ln, email, Password);
+    let student = {fn,ln,email,Password};
+    setallstudents([...allstudents,student])
+  };
+
+  return (
+    <>
+      <h1>{myNo}</h1>
+    <button onClick={()=>setMyNo(myNo+30)}>Increase</button>
+    <button onClick={()=>setMyNo(myNo-10)}>Decrease</button>
+
+      SIMPLE SCHOOL PORTAL
+       <h1>Simozy Group Of Schools</h1>
+      <input
+        type="text"
+        placeholder="First Name"
+        onChange={(event) => setfn(event.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="Last Name"
+        onChange={(event) => setln(event.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="Email"
+        onChange={(event) => setemail(event.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="Password"
+        onChange={(event) => setpassword(event.target.value)}
+      />
+      <hr />
+      <button onClick={register}>Register</button> 
+    </>
+  );
+};
+
+export default First;
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import "./index.css";
-// import './first.css'
+// import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle.js"
 import "../node_modules/bootstrap/dist/css/bootstrap.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
