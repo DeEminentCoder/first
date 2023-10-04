43a17bb102a14d663c6ec4bf062375debf6e15f8
Kehinde Olaleye Simeon <simeonkenny66@gmail.com>
2023-10-04 07:37:39 +0100

    redirect

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Home from "./components/Home";
+import NotFound from "./components/NotFound";
 
 const App = () => {
   return (
@@ -11,9 +12,11 @@ const App = () => {
       <Navbar/>
       <Routes>
         <Route path="/" element={<Home/>}/>
+        <Route path="/home" element={<Navigate to="/"/>}/>
         <Route path="/navbar" element={<Navbar/>}/>
         <Route path="/about" element={<About/>}/>
         <Route path="/contact" element={<Contact/>}/>
+        <Route path="*" element={<NotFound/>}/>
       </Routes>
     </>
   )
---
+++ src/components/NotFound.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+const NotFound = () => {
+  return (
+    <div>
+        <h2>Error</h2> 
+        <h1>404</h1>
+    </div>
+    
+  )
+}
+
+export default NotFound
