34d7545f41521cf561790a9dcab3d84d5739beaf
Kehinde Olaleye Simeon <simeonkenny66@gmail.com>
2023-10-04 07:47:33 +0100

    Error 404

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,16 @@
 
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const About = () => {
+  const navigate = useNavigate();
+  const todashboard = () => {
+  navigate("/")
+}
   return (
-    <div>About</div>
+    <div>About
+      <button onClick={todashboard}>To Dashboard</button>
+    </div>
   )
 }
 
