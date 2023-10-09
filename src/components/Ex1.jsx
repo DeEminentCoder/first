a430c28a9589948e8fe469dc905730f9a5a94ff5
Kehinde Olaleye Simeon <simeonkenny66@gmail.com>
2023-10-09 12:39:24 +0100

    del and edit

--- src/components/Ex1.jsx
+++ src/components/Ex1.jsx
@@ -10,10 +10,19 @@ const First = () => {
   const [Password, setpassword] = useState("");
   const [allstudents, setallstudents] = useState([])
   const register = () => {
-    console.log(fn, ln, email, Password);
     let student = {fn,ln,email,Password};
     setallstudents([...allstudents,student])
+    setfn("")
+    setln("")
+    setemail("")
+    setpassword("")
   };
+  
+  const delStudent = (studentIndex) => {
+    let newAllStudents = [...allstudents]
+    let found = newAllStudents.filter((student,index)=>(index!=studentIndex))
+    setallstudents(found)
+  }
 
   return (
     <>
@@ -54,12 +63,16 @@ const First = () => {
             <td>Password</td>
           </tr>
       {allstudents.map((student,index)=>(
-        <tr>
+        <tr key={index}>
           <td>{index+1}</td>
           <td>{student.fn}</td>
           <td>{student.ln}</td>
           <td>{student.email}</td>
           <td>{student.Password}</td>
+          <td>
+            <button onClick={()=>delStudent(index)} className="btn btn-danger">Del</button>
+            <button className="btn btn-warning">Edt</button>
+          </td>
         </tr>
 
       ))}
