9b5061e28ae3a86acb1cce55e908171c1bc53ac6
Kehinde Olaleye Simeon <simeonkenny66@gmail.com>
2023-10-12 10:21:54 +0100

    time

--- src/components/Ex1.jsx
+++ src/components/Ex1.jsx
@@ -104,10 +104,10 @@ const First = () => {
         <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div className="modal-body">
-        <input type="text" className="form-control my-2" value={ediedObject.fn}/>
-        <input type="text" className="form-control my-2" value={ediedObject.ln}/>
-        <input type="text" className="form-control my-2" value={ediedObject.email}/>
-        <input type="text" className="form-control my-2" value={ediedObject.Password}/>
+        <input type="text" className="form-control my-2" value={ediedObject.fn} onChange={}/>
+        <input type="text" className="form-control my-2" value={ediedObject.ln} onChange={}/>
+        <input type="text" className="form-control my-2" value={ediedObject.email} onChange={}/>
+        <input type="text" className="form-control my-2" value={ediedObject.Password} onChange={}/>
       </div>
       <div className="modal-footer">
         <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
