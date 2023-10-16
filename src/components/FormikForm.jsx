5a678dffe9d744af573ae416f22503d55a759906
Kehinde Olaleye Simeon <simeonkenny66@gmail.com>
2023-10-16 15:09:05 +0100

    import yup

--- src/components/FormikForm.jsx
+++ src/components/FormikForm.jsx
@@ -1,30 +1,55 @@
 import React from "react";
 import { useFormik } from "formik";
+import * as yup from "yup"
 const FormikForm = () => {
-    let formik = useFormik({})
+    let formik = useFormik({
+        initialValues : {
+            firstname : "",
+            lastname : "",
+            email : "",
+            password : ""
+        },
+        onSubmit : (values)=>{
+            console.log(values);
+        },
+        validationSchema : yup.Schema
+    })
   return (
     <>
       <div className="container-fluid">
         <div className="row">
-          <form action="">
+          <form action="" onSubmit={formik.handleSubmit}>
             <h1 className="text-center">Sign Up Page</h1>
             <input
               type="text"
               placeholder="First Name"
               className="form-control"
+              name="firstname"
+              value={formik.values.firstname}
+              onChange={formik.handleChange}
             />
             <input
               type="text"
               placeholder="LAst Name"
               className="form-control"
+              name="lastname"
+              value={formik.values.lastname}
+              onChange={formik.handleChange}
             />
             <input
               type="text"
               placeholder="Password"
               className="form-control"
+              name="password"
+              value={formik.values.password}
+              onChange={formik.handleChange}
             />
-            <input type="text" placeholder="Email" className="form-control" />
-            <button className="btn btn-info w-100 my-2 ">Sign Up</button>
+            <input type="text" placeholder="Email" 
+            className="form-control"
+              value={formik.values.email}
+              onChange={formik.handleChange}
+              name="email" />
+            <button type="submit" className="btn btn-info w-100 my-2 ">Sign Up</button>
           </form>
         </div>
       </div>
