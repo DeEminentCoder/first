aabce0ce940bc18fc228ac277ecfd6dba12986e1
Kehinde Olaleye Simeon <simeonkenny66@gmail.com>
2023-10-16 14:15:48 +0100

    yup

--- src/components/FormikForm.jsx
+++ src/components/FormikForm.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from "react";
 
 const FormikForm = () => {
   return (
-    <div>FormikForm</div>
-  )
-}
+    <>
+      <div className="container-fluid">
+        <div className="row">
+          <form action="">
+            <h1 className="text-center">Sign Up Page</h1>
+            <input
+              type="text"
+              placeholder="First Name"
+              className="form-control"
+            />
+            <input
+              type="text"
+              placeholder="LAst Name"
+              className="form-control"
+            />
+            <input
+              type="text"
+              placeholder="Password"
+              className="form-control"
+            />
+            <input type="text" placeholder="Email" className="form-control" />
+            <button className="btn btn-info w-100 my-2 ">Sign Up</button>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default FormikForm
+export default FormikForm;
